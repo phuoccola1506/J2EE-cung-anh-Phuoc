5bf8b9faa893392bfc4fa04e737d3016423c427f
Phuoc Cola <phuoccola1506nhp@gmail.com>
2025-05-12 00:18:35 +0700

    Bai 5

---
+++ J2EE/src/java/phuoc/dev/bai5/Bai5.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package phuoc.dev.bai5;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet("/hello-annotation")
+public class Bai5 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h2>Chào từ HelloAnnotationServlet!</h2>");
+    }
+}
